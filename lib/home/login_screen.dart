748f3f31b76e34bc1e4bce9d4ce27f42d0e834bf
Ph4nt0m <davisantana@id.uff.br>
2023-07-19 18:21:22 -0300

    agr o esquci a senha é clicavel hehe

--- lib/home/login_screen.dart
+++ lib/home/login_screen.dart
@@ -1,3 +1,233 @@
+// import 'package:flutter/material.dart';
+// import 'package:projeto_final/videos/my_videos.dart';
+
+// class LoginScreen extends StatefulWidget {
+//   @override
+//   _LoginScreenState createState() => _LoginScreenState();
+// }
+
+// class _LoginScreenState extends State<LoginScreen> {
+//   bool _showPassword = false;
+//   TextEditingController _passwordController = TextEditingController();
+//   final _formKey = GlobalKey<FormState>();
+
+//   @override
+//   void dispose() {
+//     _passwordController.dispose();
+//     super.dispose();
+//   }
+
+//   void _togglePasswordVisibility() {
+//     setState(() {
+//       _showPassword = !_showPassword;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Form(
+//         key: _formKey,
+//         child: Container(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: [
+//               const Text(
+//                 'Bem-vindo de volta! :)',
+//                 style: TextStyle(
+//                   fontSize: 24.0,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//               const SizedBox(height: 16.0),
+//               TextFormField(
+//                 decoration: const InputDecoration(
+//                   labelText: 'Usuário',
+//                 ),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Por favor, digite o seu usuário';
+//                   }
+//                   return null;
+//                 },
+//               ),
+//               const SizedBox(height: 16.0),
+//               TextFormField(
+//                 controller: _passwordController,
+//                 obscureText: !_showPassword,
+//                 decoration: InputDecoration(
+//                   labelText: 'Senha',
+//                   suffixIcon: IconButton(
+//                     icon: Icon(
+//                       _showPassword ? Icons.visibility_off : Icons.visibility,
+//                     ),
+//                     onPressed: _togglePasswordVisibility,
+//                   ),
+//                 ),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Por favor, digite a sua senha';
+//                   }
+//                   return null;
+//                 },
+//               ),
+//               const SizedBox(height: 16.0),
+//               TextButton(
+//                 onPressed: () {
+//                   // Ação para o botão "Esqueci a senha"
+//                 },
+//                 child: const Text(
+//                   'Esqueci a senha',
+//                   style: TextStyle(
+//                     color: Color.fromARGB(235, 89, 1, 1),
+//                   ),
+//                 ),
+//               ),
+//               const SizedBox(height: 16.0),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   // Ação para o botão "Entrar"
+//                   // Você pode acessar os valores dos campos usando _passwordController.text e o valor do campo de usuário
+//                   if (_formKey.currentState!.validate()) {
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                         builder: (context) => MyVideosScreen(),
+//                       ),
+//                     );
+//                   }
+//                 },
+//                 child: const Text('Entrar'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// import 'package:flutter/material.dart';
+// import 'package:projeto_final/videos/my_videos.dart';
+
+// class LoginScreen extends StatefulWidget {
+//   @override
+//   _LoginScreenState createState() => _LoginScreenState();
+// }
+
+// class _LoginScreenState extends State<LoginScreen> {
+//   bool _showPassword = false;
+//   TextEditingController _passwordController = TextEditingController();
+//   final _formKey = GlobalKey<FormState>();
+
+//   @override
+//   void dispose() {
+//     _passwordController.dispose();
+//     super.dispose();
+//   }
+
+//   void _togglePasswordVisibility() {
+//     setState(() {
+//       _showPassword = !_showPassword;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Form(
+//         key: _formKey,
+//         child: Container(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: [
+//               const Text(
+//                 'Bem-vindo de volta! :)',
+//                 style: TextStyle(
+//                   fontSize: 24.0,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//               const SizedBox(height: 16.0),
+//               TextFormField(
+//                 decoration: const InputDecoration(
+//                   labelText: 'Usuário',
+//                 ),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Por favor, digite o seu usuário';
+//                   }
+//                   return null;
+//                 },
+//               ),
+//               const SizedBox(height: 16.0),
+//               TextFormField(
+//                 controller: _passwordController,
+//                 obscureText: !_showPassword,
+//                 decoration: InputDecoration(
+//                   labelText: 'Senha',
+//                   suffixIcon: IconButton(
+//                     icon: Icon(
+//                       _showPassword ? Icons.visibility_off : Icons.visibility,
+//                     ),
+//                     onPressed: _togglePasswordVisibility,
+//                   ),
+//                 ),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Por favor, digite a sua senha';
+//                   }
+//                   return null;
+//                 },
+//               ),
+//               const SizedBox(height: 16.0),
+//               PopupMenuButton(
+//                 child: Text('Esqueci a senha'),
+//                 onSelected: (value) {
+//                   // Ação para cada opção selecionada
+//                   if (value == 'add') {
+//                     // Adicionar imagem
+//                   } else if (value == 'remove') {
+//                     // Remover imagem
+//                   }
+//                 },
+//                 itemBuilder: (context) => [
+//                   PopupMenuItem(
+//                     child: Text('Adicionar imagem'),
+//                     value: 'add',
+//                   ),
+//                   PopupMenuItem(
+//                     child: Text('Remover imagem'),
+//                     value: 'remove',
+//                   ),
+//                 ],
+//               ),
+//               const SizedBox(height: 16.0),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   // Ação para o botão "Entrar"
+//                   // Você pode acessar os valores dos campos usando _passwordController.text e o valor do campo de usuário
+//                   if (_formKey.currentState!.validate()) {
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                         builder: (context) => MyVideosScreen(),
+//                       ),
+//                     );
+//                   }
+//                 },
+//                 child: const Text('Entrar'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:projeto_final/videos/my_videos.dart';
 
@@ -73,16 +303,32 @@ class _LoginScreenState extends State<LoginScreen> {
                 },
               ),
               const SizedBox(height: 16.0),
-              TextButton(
-                onPressed: () {
-                  // Ação para o botão "Esqueci a senha"
+              PopupMenuButton(
+                child: Text('Esqueci a senha'),
+                onSelected: (value) {
+                  // Ação para cada opção selecionada
+                  if (value == 'try') {
+                    // Exibir um SnackBar com a mensagem "Tente lembrar"
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Tente lembrar'),
+                        duration: Duration(seconds: 2),
+                      ),
+                    );
+                  } else if (value == 'reset') {
+                    // Resetar a senha
+                  }
                 },
-                child: const Text(
-                  'Esqueci a senha',
-                  style: TextStyle(
-                    color: Color.fromARGB(235, 89, 1, 1),
+                itemBuilder: (context) => [
+                  PopupMenuItem(
+                    child: Text('Tentar lembrar'),
+                    value: 'try',
                   ),
-                ),
+                  //   PopupMenuItem(
+                  //     child: Text('Resetar senha'),
+                  //     value: 'reset',
+                  //   ),
+                ],
               ),
               const SizedBox(height: 16.0),
               ElevatedButton(
