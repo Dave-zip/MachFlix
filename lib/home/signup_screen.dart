f4824993a4a88886f2eed4c0d4d22dcbed0b2c24
GuilhermeCSRamos <gcsramos@id.uff.br>
2023-07-20 23:10:51 -0300

    adicionado verificacao de email ja registrado

--- lib/home/signup_screen.dart
+++ lib/home/signup_screen.dart
@@ -1,79 +1,53 @@
 import 'package:flutter/material.dart';
-
 import '../database/banco.dart';
-// import '../database/banco.dart';
-
-// class SignupScreen extends StatefulWidget {
-//   @override
-//   _SignupScreenState createState() => _SignupScreenState();
-// }
-
-// class _SignupScreenState extends State<SignupScreen> {
-//   final _formKey = GlobalKey<FormState>();
-
-//   final _nomeController = TextEditingController();
-//   final _emailController = TextEditingController();
-//   final _senhaController = TextEditingController();
-
-//   Future<void> _cadastrarUsuario() async {
-//     if (_formKey.currentState!.validate()) {
-//       String email = _emailController.text;
-
-//       // Verificar se o email já existe no banco de dados
-//       bool emailExists = await _verificarEmailExistente(email);
-
-//       if (emailExists) {
-//         // Se o email já existir, exibir uma mensagem de erro e não realizar o cadastro.
-//         showDialog(
-//           context: context,
-//           builder: (context) {
-//             return AlertDialog(
-//               title: Text('Erro'),
-//               content: Text(
-//                   'O email já está cadastrado. Por favor, insira um email diferente.'),
-//               actions: [
-//                 TextButton(
-//                   onPressed: () {
-//                     Navigator.pop(context);
-//                   },
-//                   child: Text('Ok'),
-//                 ),
-//               ],
-//             );
-//           },
-//         );
-//       } else {
-//         // Se o email não existir no banco de dados, podemos prosseguir com o cadastro.
-//         Map<String, dynamic> novoUsuario = {
-//           'name': _nomeController.text,
-//           'email': email,
-//           'password': _senhaController.text,
-//         };
-
-//         await DatabaseHelper.instance.insertData(novoUsuario);
-
-//         // Exibir a mensagem de cadastro bem-sucedido usando um SnackBar
-//         ScaffoldMessenger.of(context).showSnackBar(
-//           SnackBar(
-//             content: Text('Cadastro realizado com sucesso!'),
-//             duration: Duration(seconds: 2),
-//           ),
-//         );
-//       }
-//     }
-//   }
-
-//   Future<bool> _verificarEmailExistente(String email) async {
-//     List<Map<String, dynamic>> usuarios =
-//         await DatabaseHelper.instance.getAllData();
-//     return usuarios.any((usuario) => usuario['email'] == email);
-//   }
 
 class SignupScreen extends StatelessWidget {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _cadastrarUsuario(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      String name = _nameController.text;
+      String email = _emailController.text;
+      String password = _passwordController.text;
+
+      bool emailExists = await _verificarEmailExistente(email);
+
+      if (emailExists) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+                'O email já está cadastrado. Por favor, insira um email diferente.'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+      } else {
+        Map<String, dynamic> novoUsuario = {
+          'name': name,
+          'email': email,
+          'password': password,
+        };
+
+        await DatabaseHelper.instance.insertData(novoUsuario);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+                'Cadastro realizado com sucesso! Nome: $name, Email: $email'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+      }
+    }
+  }
+
+  Future<bool> _verificarEmailExistente(String email) async {
+    List<Map<String, dynamic>> usuarios =
+        await DatabaseHelper.instance.getAllData();
+    return usuarios.any((usuario) => usuario['email'] == email);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -135,33 +109,7 @@ class SignupScreen extends StatelessWidget {
               ),
             ),
             ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  _formKey.currentState!.save();
-                  String name = _nameController.text;
-                  String email = _emailController.text;
-                  String password = _passwordController.text;
-
-                  DatabaseHelper.instance.insertData(
-                      {"name": name, "email": email, "password": password});
-
-                  print('Nome: $name');
-                  print('Email: $email');
-                  print('Senha: $password');
-
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(
-                          'Cadastro realizado com sucesso! Nome: $name, Email: $email'),
-                      action: SnackBarAction(
-                          label: 'Desfazer',
-                          onPressed: () {
-                            print('Cadastro desfeito');
-                          }),
-                    ),
-                  );
-                }
-              },
+              onPressed: () => _cadastrarUsuario(context),
               child: const Text('Cadastrar'),
             ),
           ],
